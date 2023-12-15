91951888dc641aae710af89b99c760e34e13a96e
XGD1119363048 <1119363048@qq.com>
2023-12-15 16:04:29 +0800

    import antd

--- src/views/sandbox/home/Home.js
+++ src/views/sandbox/home/Home.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Button } from 'antd'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Button type='primary'>Button</Button>
+    </div>
   )
 }
