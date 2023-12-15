82227787aeca192473c583aa50eefa2f0c316d69
许国栋(Guodong Xu) <gdxu@univista-isg.com>
2023-12-15 11:28:56 +0800

    some-routes-1

--- src/router/IndexRouter.js
+++ src/router/IndexRouter.js
@@ -3,6 +3,11 @@ import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Login from '../views/login/Login'
 import NewsSandBox from '../views/sandbox/NewsSandBox'
 
+import Home from '../views/sandbox/home/Home'
+import UserList from '../views/sandbox/user-manage/UserList'
+import RoleList from '../views/sandbox/right-manage/RoleList'
+import RightList from '../views/sandbox/right-manage/RightList'
+
 export default function IndexRouter() {
   return (
     <HashRouter>
@@ -10,7 +15,13 @@ export default function IndexRouter() {
         <Route path='/login' element={<Login />} />
         <Route path='/' element={<AuthComponent>
           <NewsSandBox />
-        </AuthComponent>} />
+        </AuthComponent>}>
+          <Route index element={<Navigate to='home' />} />
+          <Route path='home' element={<Home />} />
+          <Route path='user-manage/list' element={<UserList />} />
+          <Route path='right-manage/role/list' element={<RoleList />} />
+          <Route path='right-manage/right/list' element={<RightList />} />
+        </Route>
       </Routes>
     </HashRouter>
   )
--- src/views/sandbox/NewsSandBox.js
+++ src/views/sandbox/NewsSandBox.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import SideMenu from '../../components/sandbox/SideMenu'
+import TopHeader from '../../components/sandbox/TopHeader'
+
+import { Outlet } from 'react-router-dom'
 
 export default function NewsSandBox() {
   return (
-    <div>NewsSandBox</div>
+    <div>
+      <SideMenu />
+      <TopHeader />
+
+      <Outlet />
+    </div>
   )
 }
