376f69f8a0a191805f9e57518855423541a4753e
XGD1119363048 <1119363048@qq.com>
2023-12-15 17:42:21 +0800

    sider

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -1,38 +1,57 @@
+import React from 'react'
+import './index.css'
 import { Layout, Menu } from 'antd';
 import {
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
+  UserOutlined
 } from '@ant-design/icons';
-import React from 'react'
-
 const { Sider } = Layout;
+
+const menuList = [
+  {
+    key: '/home',
+    label: '首页',
+    icon: <UserOutlined />
+  },
+  {
+    key: '/user-manage',
+    label: '用户管理',
+    icon: <UserOutlined />,
+    children: [
+      {
+        key: '/user-manage/list',
+        label: '用户列表',
+        icon: <UserOutlined />
+      }
+    ]
+  },
+  {
+    key: '/right-manage',
+    label: '权限管理',
+    icon: <UserOutlined />,
+    children: [
+      {
+        key: '/right-manage/role/list',
+        label: '角色列表',
+        icon: <UserOutlined />
+      },
+      {
+        key: '/right-manage/right/list',
+        label: '权限列表',
+        icon: <UserOutlined />
+      }
+    ]
+  }
+]
 
 export default function SideMenu() {
   return (
-    <Sider trigger={null} collapsible>
-      <div className="demo-logo-vertical" />
+    <Sider trigger={null} collapsible collapsed={false}>
+      <div className="demo-logo-vertical">全球新闻发布管理系统</div>
       <Menu
         theme="dark"
         mode="inline"
-        defaultSelectedKeys={['1']}
-        items={[
-          {
-            key: '1',
-            icon: <UserOutlined />,
-            label: 'nav 1',
-          },
-          {
-            key: '2',
-            icon: <VideoCameraOutlined />,
-            label: 'nav 2',
-          },
-          {
-            key: '3',
-            icon: <UploadOutlined />,
-            label: 'nav 3',
-          },
-        ]}
+        defaultSelectedKeys={['/home']}
+        items={menuList}
       />
     </Sider>
   )
