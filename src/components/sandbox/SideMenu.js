68eb5a6f18ba5fda1f692f89d40cbb2e785f92c8
XGD1119363048 <1119363048@qq.com>
2023-12-16 14:20:53 +0800

    db file & sideMenu data render

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import './index.css'
 import { Layout, Menu } from 'antd';
@@ -7,45 +8,57 @@ import {
 } from '@ant-design/icons';
 const { Sider } = Layout;
 
-const menuList = [
-  {
-    key: '/home',
-    label: '首页',
-    icon: <UserOutlined />
-  },
-  {
-    key: '/user-manage',
-    label: '用户管理',
-    icon: <UserOutlined />,
-    children: [
-      {
-        key: '/user-manage/list',
-        label: '用户列表',
-        icon: <UserOutlined />
-      }
-    ]
-  },
-  {
-    key: '/right-manage',
-    label: '权限管理',
-    icon: <UserOutlined />,
-    children: [
-      {
-        key: '/right-manage/role/list',
-        label: '角色列表',
-        icon: <UserOutlined />
-      },
-      {
-        key: '/right-manage/right/list',
-        label: '权限列表',
-        icon: <UserOutlined />
-      }
-    ]
-  }
-]
+const iconList = {
+  '/home': <UserOutlined />,
+
+  '/user-manage': <UserOutlined />,
+  '/user-manage/list': <UserOutlined />,
+
+  '/right-manage': <UserOutlined />,
+  '/right-manage/role/list': <UserOutlined />,
+  '/right-manage/right/list': <UserOutlined />,
+
+  '/news-manage': <UserOutlined />,
+  '/news-manage/add': <UserOutlined />,
+  '/news-manage/draft': <UserOutlined />,
+  '/news-manage/category': <UserOutlined />,
+
+  '/audit-manage': <UserOutlined />,
+  '/audit-manage/audit': <UserOutlined />,
+  '/audit-manage/list': <UserOutlined />,
+
+  '/publish-manage': <UserOutlined />,
+  '/publish-manage/unpublished': <UserOutlined />,
+  '/publish-manage/published': <UserOutlined />,
+  '/publish-manage/sunset': <UserOutlined />
+}
 
 export default function SideMenu(props) {
   const navigate = useNavigate()
+  useEffect(() => {
+    axios.get('http://localhost:5000/rights?_embed=children').then(res => {
+      let menuList = []
+      res.data.forEach(item => {
+        if (item.pagepermisson === 1) {
+          let tmpMenu = {
+            key: item.key,
+            label: item.title,
+            icon: iconList[item.key]
+          }
+          if (item.children && item.children.length > 0) {
+            tmpMenu.children = item.children.filter(child => child.pagepermisson === 1).map(child => ({
+              key: child.key,
+              label: child.title,
+              icon: iconList[child.key]
+            }))
+          }
+          menuList.push(tmpMenu)
+        }
+      })
+      setMenu(menuList)
+    })
+  }, [])
+  const [menu, setMenu] = useState([])
   return (
     <Sider trigger={null} collapsible collapsed={false}>
       <div className="demo-logo-vertical">全球新闻发布管理系统</div>
@@ -53,7 +66,7 @@ export default function SideMenu(props) {
         theme="dark"
         mode="inline"
         defaultSelectedKeys={['/home']}
-        items={menuList}
+        items={menu}
         onClick={({key}) => {
           navigate(key)
         }}
--- src/views/sandbox/home/Home.js
+++ src/views/sandbox/home/Home.js
@@ -1,10 +1,28 @@
 import React from 'react'
+// import axios from 'axios'
 import { Button } from 'antd'
 
 export default function Home() {
   return (
     <div>
-      <Button type='primary'>Button</Button>
+      <Button type='primary' onClick={() => {
+        // axios.get('http://localhost:8000/posts').then(res => {
+        //   console.log(res)
+        // })
+
+        // axios.post('http://localhost:8000/posts', {
+        //   title: '22222',
+        //   author: 'xgd'
+        // })
+
+        // axios.get('http://localhost:8000/posts?_embed=comments').then(res => {
+        //   console.log(res.data)
+        // })
+
+        // axios.get('http://localhost:8000/comments?_expand=post').then(res => {
+        //   console.log(res.data)
+        // })
+      }}>Button</Button>
     </div>
   )
 }
