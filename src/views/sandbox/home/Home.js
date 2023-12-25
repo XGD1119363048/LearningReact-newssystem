c807a719badb5f8928bb4198c22d7990f6583a14
许国栋(Guodong Xu) <gdxu@univista-isg.com>
2023-12-25 16:52:45 +0800

    home排版

--- src/views/sandbox/home/Home.js
+++ src/views/sandbox/home/Home.js
@@ -1,28 +1,53 @@
 import React from 'react'
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 // import axios from 'axios'
-import { Button } from 'antd'
+import { Avatar, Card, Col, List, Row } from 'antd'
+const { Meta } = Card;
 
 export default function Home() {
   return (
     <div>
-      <Button type='primary' onClick={() => {
-        // axios.get('http://localhost:8000/posts').then(res => {
-        //   console.log(res)
-        // })
-
-        // axios.post('http://localhost:8000/posts', {
-        //   title: '22222',
-        //   author: 'xgd'
-        // })
-
-        // axios.get('http://localhost:8000/posts?_embed=comments').then(res => {
-        //   console.log(res.data)
-        // })
-
-        // axios.get('http://localhost:8000/comments?_expand=post').then(res => {
-        //   console.log(res.data)
-        // })
-      }}>Button</Button>
+      <Row gutter={16}>
+        <Col span={8}>
+          <Card title="用户最常浏览">
+            <List
+              size="small"
+              dataSource={['111', '222', '333']}
+              renderItem={(item) => <List.Item>{item}</List.Item>}
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="用户点赞最多">
+            <List
+              size="small"
+              dataSource={['111', '222', '333']}
+              renderItem={(item) => <List.Item>{item}</List.Item>}
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card
+            cover={
+              <img
+                alt="example"
+                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+              />
+            }
+            actions={[
+              <SettingOutlined key="setting" />,
+              <EditOutlined key="edit" />,
+              <EllipsisOutlined key="ellipsis" />,
+            ]}
+          >
+            <Meta
+              avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />}
+              title="Card title"
+              description="This is the description"
+            />
+          </Card>
+        </Col>
+      </Row>
     </div>
   )
 }
