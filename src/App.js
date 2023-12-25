6ab480f8b198c84a9ccb83084e9cc0ae6fa09045
许国栋(Guodong Xu) <gdxu@univista-isg.com>
2023-12-25 16:39:30 +0800

    redux persist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import IndexRouter from "./router/IndexRouter"
 import { Provider } from 'react-redux'
-import store from "./redux/store"
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from "./redux/store"
 
 // css
 import './App.css'
 function App() {
   return <Provider store={store}>
-    <IndexRouter></IndexRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <IndexRouter></IndexRouter>
+    </PersistGate>
   </Provider>
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,25 @@
 import { combineReducers } from 'redux'
 import { createStore } from 'redux'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import { CollapsedReducer } from './reducers/CollapsedReducer'
 import { LoadingReducer } from './reducers/LoadingReducer'
+
+const persistConfig = {
+  key: 'xgd',
+  storage,
+  whitelist: ['CollapsedReducer']
+}
 
 const reducer = combineReducers({
   CollapsedReducer,
   LoadingReducer
 })
 
-const store = createStore(reducer)
+const persistedReducer = persistReducer(persistConfig, reducer)
 
-export default store
+const store = createStore(persistedReducer)
+let persistor = persistStore(store)
+
+export {store, persistor}
